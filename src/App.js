fdf45573ad63872d14035c28e573f10570c7d860
David Ryan <djrdev@icloud.com>
2017-12-05 15:54:24 -0700

    initial html structure

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      scroll: null,
+      people: null,
+      planets: null,
+      vehicles: null,
+      display: ''
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <aside></aside>
+        <div className="main-container">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <body>
+            
+          </body>
+        </div>
       </div>
     );
   }
