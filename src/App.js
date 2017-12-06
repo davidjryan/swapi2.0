d95fe5a2d75eae83d7df21f7b9890dcd93afa32a
David Ryan <djrdev@icloud.com>
2017-12-05 19:37:01 -0700

    html & CSS mock completed

--- src/App.js
+++ src/App.js
@@ -14,18 +14,68 @@ class App extends Component {
       display: ''
     }
   }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className="App">
           <aside></aside>
         <div className="main-container">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Star Wars</h1>
+            <button className="favorites">Favorites<span>10</span></button>
           </header>
-          <body>
-            
-          </body>
+          <hr />
+          <nav className="Data-nav">
+            <button className="People-button">people</button>
+            <button className="Planets-button">planets</button>
+            <button className="Vehicles-button">vehicles</button>
+          </nav>
+          <section className="Card-container">
+            <article className="Card">
+              <header className="Card-header">
+                <p className="Card-title">Luke Skywalker</p>
+                <button className="Card-button">IMG</button>
+              </header>
+              <hr />
+              <p>Tatooine</p>
+              <p>Jedi</p>
+              <p>Moisture Farmer</p>
+            </article>
+            <article className="Card">
+              <header className="Card-header">
+                <p className="Card-title">Luke Skywalker</p>
+                <button className="Card-button">IMG</button>
+              </header>
+              <hr />
+              <p>Tatooine</p>
+              <p>Jedi</p>
+              <p>Moisture Farmer</p>
+            </article>
+            <article className="Card">
+              <header className="Card-header">
+                <p className="Card-title">Luke Skywalker</p>
+                <button className="Card-button">IMG</button>
+              </header>
+              <hr />
+              <p>Tatooine</p>
+              <p>Jedi</p>
+              <p>Moisture Farmer</p>
+            </article>
+            <article className="Card">
+              <header className="Card-header">
+                <p className="Card-title">Luke Skywalker</p>
+                <button className="Card-button">IMG</button>
+              </header>
+              <hr />
+              <p>Tatooine</p>
+              <p>Jedi</p>
+              <p>Moisture Farmer</p>
+            </article>
+          </section>
         </div>
       </div>
     );
