a867470b4eb494077f0180d1fa3754e1cbb0e813
David Ryan <djrdev@icloud.com>
2017-12-06 19:11:55 -0700

    finished getCrawl and destructured data into returned object

--- src/helper.js
+++ src/helper.js
@@ -10,10 +10,19 @@ export async function getVehicles() {
 
 }
 
-export async function getScroll() {
-  const fetchScroll = await fetch('https://swapi.co/api/films/')
-  const films = await fetchScroll.json()
-  debugger;
+export async function getCrawl(number) {
+  const film = await fetch(`https://swapi.co/api/films/${number}`)
+  const filmData = await film.json()
+  const { title, opening_crawl, release_date} = filmData;
 
-  return films
+
+  return {title, crawl: opening_crawl, date: release_date}
+}
+
+// export async function getAllData(crawl, people, planets, vehicles) {
+//   return Promise.
+// }
+
+export function randomizer() {
+  return Math.floor(Math.random() * 6) + 1;
 }
