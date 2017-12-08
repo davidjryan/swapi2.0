9080578d6c8b3a5438b6e6158d8f95a1ad6089a0
David Ryan <djrdev@icloud.com>
2017-12-07 23:41:09 -0700

    broke out Nav component

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+
+  return (
+    <nav className="Data-nav">
+      <button className="People-button">people</button>
+      <button className="Planets-button">planets</button>
+      <button className="Vehicles-button">vehicles</button>
+    </nav>
+  )
+}
+
+export default Nav;
