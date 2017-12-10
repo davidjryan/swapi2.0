7587950a09219cef2ed3837f173e89c10df913f7
David Ryan <djrdev@icloud.com>
2017-12-10 12:30:07 -0700

    added favorites button to Nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -7,6 +7,7 @@ const Nav = () => {
       <button className="People-button">people</button>
       <button className="Planets-button">planets</button>
       <button className="Vehicles-button">vehicles</button>
+      <button className="favorites">favorites<span>10</span></button>
     </nav>
   )
 }
